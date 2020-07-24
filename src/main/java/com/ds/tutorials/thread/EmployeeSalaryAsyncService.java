a0f33fc70955064d91176e2c6c75428489159aef
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-07-24 14:33:58 +0530

    Add comments

--- src/main/java/com/ds/tutorials/thread/EmployeeSalaryAsyncService.java
+++ src/main/java/com/ds/tutorials/thread/EmployeeSalaryAsyncService.java
@@ -3,14 +3,17 @@ package com.ds.tutorials.thread;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.concurrent.CompletableFuture;
 
+//TODO - @Async has two limitations
+//it must be applied to public methods only
+//self-invocation – calling the async method from within the same class – won't work
+//The reasons are simple – the method needs to be public so that it can be proxied. And self-invocation doesn't work because it bypasses the proxy and calls the underlying method directly.
 @Service
 public class EmployeeSalaryAsyncService {
 
     @Async
-    public CompletableFuture<EmployeeSalary> fetchSalary(String empId ){
+    public CompletableFuture<EmployeeSalary> fetchSalary(String empId) {
         EmployeeSalary employeeSalary = new EmployeeSalary();
         employeeSalary.setEmpId(empId);
         employeeSalary.setSalary(20000);
